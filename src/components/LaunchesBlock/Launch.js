a89ac53e2a1b1c19bcc9f39c229fae014e9d6d56
Omnitole <tkkirienko@gmail.com>
2023-10-24 18:04:53 +0300

    image problem fixed

--- src/components/LaunchesBlock/Launch.js
+++ src/components/LaunchesBlock/Launch.js
@@ -1,9 +1,9 @@
 const Launch = ({launch}) => {
-    const {mission_name:name,launch_year:year,links: {mission_patch_small}} = launch
+    const {mission_name:name,launch_year:year,links:{mission_patch_small:image}} = launch
     return (
         <div>
             <div>Mission name - {name}, year of launch - {year}</div>
-            <img src={mission_patch_small} alt={name}/>
+            <img src={image} alt={name}/>
         </div>
     );
 };
